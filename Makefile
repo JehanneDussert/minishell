1a2360f3be432b3055405d92b85100aaaa1d487b
Emma <emmalindi@gmail.com>
2020-10-06 18:01:13 +0200

    makefile and file organization, includes

--- Makefile
+++ Makefile
@@ -1,1 +1,34 @@
+NAME = minishell
+CC = gcc
+C_FLAGS = -Wall -Werror
 
+ERROR_PATH = ms_error/
+ERROR_FILES = 
+ERROR =
+
+EXECUTION_PATH = ms_execution/
+EXECUTION_FILES = 
+EXECUTION =
+
+PARSING_PATH = ms_parsing/
+PARSING_FILES =
+PARSING =
+
+SRCS_PATH = srcs/
+SRCS_FILES = ${ERROR} ${EXECUTION} ${PARSING}
+SRCS =
+
+OBJS = ${SRCS:.c=.o}
+
+$(NAME):	${OBJS}
+		${CC}
+
+all:	${NAME}
+
+clean:
+	rm -f ${OBJS}
+
+fclean:	clean
+	rm -f ${NAME}
+
+make re: fclean all
---
+++ srcs/includes/minishell.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/06 17:28:08 by marvin            #+#    #+#             */
+/*   Updated: 2020/10/06 17:28:08 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include "structures.h"
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <sys/stat.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <signal.h>
+# include <dirent.h>
+# include <string.h>
+# include <errno.h>
+
+#endif
