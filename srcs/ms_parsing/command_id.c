8fc0276ac87ac920969f7e530ecd77a10f90b7cf
Emma <emmalindi@gmail.com>
2020-10-19 12:14:10 +0200

    avancement sur la structure de command exec et addition de pipes_id pour les pipes + commentaires sur les free

--- srcs/ms_parsing/command_id.c
+++ srcs/ms_parsing/command_id.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   command_id.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ede-banv <ede-banv@student.42.fr>          +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/13 16:33:36 by ede-banv          #+#    #+#             */
-/*   Updated: 2020/10/13 16:49:23 by ede-banv         ###   ########.fr       */
+/*   Updated: 2020/10/19 12:13:19 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,12 @@
 
 char	*command_id(char **comm)
 {
+    //identifier la commande selon le 1er token
     return(*comm);
+}
+
+char    *pipes_id(char **comm)
+{
+    //cette fonction va se servir de command id donc cest dans comand id que le debut de l'excecution des commandes va se faire
+    return (*comm);
 }
