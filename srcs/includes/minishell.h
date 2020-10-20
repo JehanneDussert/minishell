abdcaeac96138def859cfb1c5377817c41cdddfe
Emma DE BANVILLE <ede-banv@e2r12p8.42.fr>
2020-10-20 16:44:45 +0200

    pwd done, added prefixes to makefile

--- srcs/includes/minishell.h
+++ srcs/includes/minishell.h
@@ -66,6 +66,7 @@ void	error_msg(char *bin, char *message)
 char	*command_id(char **comm, t_exit *exit);;
 void	ft_echo(char **comm);
 void	cd_id(char **comm);
+void	pwd_id(char **cmd);
 
 /*
 **FREE
