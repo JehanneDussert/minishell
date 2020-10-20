03ac1135eb684bee5651cfe5c1c20ede94b1b7ce
Emma DE BANVILLE <ede-banv@e2r12p8.42.fr>
2020-10-20 11:39:13 +0200

    avancement sur pipes_id

--- srcs/ms_parsing/command_id.c
+++ srcs/ms_parsing/command_id.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   command_id.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: ede-banv <ede-banv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/13 16:33:36 by ede-banv          #+#    #+#             */
-/*   Updated: 2020/10/19 17:58:09 by marvin           ###   ########.fr       */
+/*   Updated: 2020/10/20 11:38:50 by ede-banv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-char	*command_id(char **comm, t_exit *exit)//puisque je fais read 
+char	*command_id(char **comm, t_exit *exit)//puisque je fais read
 {
     //identifier la commande selon le 1er token
     if (!ft_strncmp(comm[0], "echo", 4))
@@ -40,6 +40,7 @@ char	*command_id(char **comm, t_exit *exit)//puisque je fais read
 char	*pipes_id(t_cmd **cmd, t_exit *exit)
 {
     int i;
+    int status;
     //cette fonction va se servir de command id donc cest dans comand id que le debut de l'excecution des commandes va se faire
     //si ca se sert de comm_id
 
@@ -57,17 +58,21 @@ char	*pipes_id(t_cmd **cmd, t_exit *exit)
         cmd[i]->pid = fork();
         if (cmd[i]->pid == 0)
         {
+            if (i != 0)
+                dup2(cmd[i - 1]->pipe[0], 0);
             dup2(cmd[i]->pipe[1], 1);
             command_id(cmd[i]->cmd, exit);
         }
         else if (cmd[i]->pid == -1)
-            ;//erreur de fork 
+            ;//erreur de fork
         i++;
     }
     i = 0;
     while (cmd[i]->cmd)
     {
-        waitpid(cmd[i]->pid);//a voir la redaction
+        waitpid(cmd[i]->pid, &status, 0);//a voir la redaction
+        close(cmd[i]->pipe[0]);
+        close(cmd[i]->pipe[1]);
         //voir comment gerer les bails de pipes
         i++;
     }
