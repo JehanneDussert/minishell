3c4ac8745751dca42778922436112d2a2fb12ebf
Jehanne Dussert <jehanne-dussert@hotmail.fr>
2020-10-21 16:37:57 +0200

    info for exec.c

--- srcs/ms_parsing/exec.c
+++ srcs/ms_parsing/exec.c
@@ -6,22 +6,26 @@
 /*   By: jdussert <jdussert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/21 14:53:40 by jdussert          #+#    #+#             */
-/*   Updated: 2020/10/21 15:43:06 by jdussert         ###   ########.fr       */
+/*   Updated: 2020/10/21 16:37:40 by jdussert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
 // int execve(const char *filename, char *const *argv[], char *const *envp)
-// never return execpt if it fails, return -1 
+// never return except if it fails, return -1 
 
 // filename : le nom de notre executable
 // argv[] : tableau de chaines d'arguments qui doit se terminer par un pointeur NULL
 // envp : environnement du nouveau programme qui doit se terminer par un pointeur NULL
 
-int     ft_exec(char *filename)
+int     ft_exec(char **comm)
 {
-    if (execve(filename) == -1)
+    // recuperer variables d'environnement dans liste chainee : tt ce qui est dans env
+    // chercher si binaire existe dans PATH (cf env PATH) -> PATH a save dans char *
+    // chercher l'executable, /bin/ls
+    // envoyer la commande **comm
+    if (execve() == -1)
         return (-1);
     return (0);
 }
