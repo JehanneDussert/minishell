f0b00e08a7df9a8cc0ac7a00f37c63142afd6427
Jehanne Dussert <jehanne-dussert@hotmail.fr>
2020-10-26 12:37:18 +0100

    norme utils

--- srcs/ms_utils/welcome_bye.c
+++ srcs/ms_utils/welcome_bye.c
@@ -3,26 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   welcome_bye.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ede-banv <ede-banv@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jdussert <jdussert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/20 17:16:59 by ede-banv          #+#    #+#             */
-/*   Updated: 2020/10/21 12:29:33 by ede-banv         ###   ########.fr       */
+/*   Updated: 2020/10/26 12:36:18 by jdussert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-void    welcomer(void) //font from http://patorjk.com/software/taag/#p=display&h=1&v=1&f=Banner3&t=Minishell
+void	welcomer(void) //font from http://patorjk.com/software/taag/#p=display&h=1&v=1&f=Banner3&t=Minishell
 {
-    ft_putstr_fd("Welcome to our\n", 1);
-    ft_putstr_fd("##     ## #### ##    ## ####  ######  ##     ## ######## ##       ##\n", 1);
-    ft_putstr_fd("###   ###  ##  ###   ##  ##  ##    ## ##     ## ##       ##       ##\n", 1);
-    ft_putstr_fd("#### ####  ##  ####  ##  ##  ##       ##     ## ##       ##       ##\n", 1);
-    ft_putstr_fd("## ### ##  ##  ## ## ##  ##   ######  ######### ######   ##       ##\n", 1);
-    ft_putstr_fd("##     ##  ##  ##  ####  ##        ## ##     ## ##       ##       ##\n", 1);
-    ft_putstr_fd("##     ##  ##  ##   ###  ##  ##    ## ##     ## ##       ##       ##\n", 1);
-    ft_putstr_fd("##     ## #### ##    ## ####  ######  ##     ## ######## ######## ########\n", 1);
-    ft_putchar_fd('\n', 1);
+	ft_putendl_fd("Welcome to our", 1);
+	ft_putstr_fd("##     ## #### ##    ## ####  ######  ##     ", 1);
+	ft_putendl_fd("## ######## ##       ##", 1);
+	ft_putstr_fd("###   ###  ##  ###   ##  ##  ##", 1);
+	ft_putendl_fd("    ## ##     ## ##       ##       ##", 1);
+	ft_putstr_fd("#### ####  ##  ####  ##  ##  ##       ##     ", 1);
+	ft_putendl_fd("## ##       ##       ##", 1);
+	ft_putstr_fd("## ### ##  ##  ## ## ##  ##   ######  ", 1);
+	ft_putendl_fd("######### ######   ##       ##", 1);
+	ft_putstr_fd("##     ##  ##  ##  ####  ##        ## ##     ", 1);
+	ft_putendl_fd("## ##       ##       ##", 1);
+	ft_putstr_fd("##     ##  ##  ##   ###  ##  ##    ## ##     ", 1);
+	ft_putendl_fd("## ##       ##       ##", 1);
+	ft_putstr_fd("##     ## #### ##    ## ####  ######  ##     ", 1);
+	ft_putendl_fd("## ######## ######## ########", 1);
 }
 
 void	byebye(void)
