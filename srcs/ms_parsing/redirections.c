ce03751c533f0a347ac5645af39908d50fd9cdfc
Jehanne DUSSERT <jdussert@student.42.fr>
2020-11-03 11:35:33 +0100

    need : truncate comd after redirection + file's rights

--- srcs/ms_parsing/redirections.c
+++ srcs/ms_parsing/redirections.c
@@ -12,39 +12,37 @@
 
 #include "../includes/minishell.h"
 
-void	ft_create_new_file(char *comd, t_all *all, int *i)
+void	ft_create_new_file(char *comd, t_all *all)
 {
-	(*i)++;
-	all->fd = open(comd, O_CREAT | O_WRONLY);
+	all->fd = open(comd, O_CREAT | O_RDWR);
 }
-
-void	ft_write_new_comd(char *comd, t_all *all, int *i)
+/*
+void	ft_write_new_comd(char *comd, t_all *all)
 {
-	(*i)++;
 	;// add at the end of file
 }
 
-void	ft_replace_file(char *comd, t_all *all, int *i)
+void	ft_replace_file(char *comd, t_all *all)
 {
-	i++;
 	;// rm & write a new file
-}
+}*/
 
 void	ft_redirections(char **comd, t_all *all)
 {
 	int	i;
 
 	i = 0;
+	all->fd = 0;
 	while (comd[i])
 	{
 		if ((comd[i][0] == '>' || comd[i][0] == '<') && (all->fd = open(comd[i], O_WRONLY)) == -1)
-			ft_create_new_file(comd[i], all, &i);
+			ft_create_new_file(comd[++i], all);
 		else if (comd[i][0] == '>' && comd[i][1] == '>' && (all->fd = open(comd[i], O_WRONLY)) != -1)
-			ft_write_new_comd(comd[i], all, &i);
+			;//ft_write_new_comd(comd[i], all);
 		else if (comd[i][0] == '<' && (all->fd = open(comd[i], O_WRONLY)) != -1)
-			ft_replace_file(comd[i], all, &i);
+			;//ft_replace_file(comd[i], all);
 		i++;
 	}
-	/*dup2(all->fd, 1);*/
+	dup2(all->fd, 1);
 	close(all->fd);
 }
