2beb73087b6c5f99958c2c315f5e9765447f7ce1
ede-banv <ede-banv@student.42.fr>
2021-01-03 17:59:29 +0000

    pwd_exist normed. 1 file too many in cd. to do: norm olddir and see if protect

--- srcs/ms_parsing/cd.c
+++ srcs/ms_parsing/cd.c
@@ -6,7 +6,7 @@
 /*   By: ubuntu <ubuntu@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/20 13:27:08 by ede-banv          #+#    #+#             */
-/*   Updated: 2021/01/03 17:01:14 by ubuntu           ###   ########.fr       */
+/*   Updated: 2021/01/03 17:58:34 by ubuntu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,41 +36,47 @@ void	cd_home(t_all *all, int *catch)
 	}
 }
 
-int		pwdexist(t_all *all, int *catch, int mode, t_lst *tmp)
+int		pwd_error(t_all *all, int *catch, int mode)
 {
-	char	*buf;
-	int		used;
-
-	used = 0;
-	if (!(buf = ft_calloc(sizeof(buf), 1024)))
+	if (mode == 0)
 	{
 		all->err = 1;
 		*catch = 1;
 		ft_malloc_error("cd");
-		return (0);
 	}
-	if (!tmp->content && mode == 1)//mode 1 : cd -
+	else
 	{
 		all->err = 1;
 		*catch = 1;
 		error_msg("cd", "OLDPWD not set");
 	}
+}
+
+int		pwdexist(t_all *all, int *catch, int mode, t_lst *tmp)
+{
+	char	*buf;
+
+	if (!(buf = ft_calloc(sizeof(buf), 1024)))
+	{
+		pwd_error(all, catch, 0);
+		return (0);
+	}
+	if (!tmp->content && mode == 1)//mode 1 : cd -
+		pwd_error(all, catch, 1);
 	else if (tmp->content && mode == 1)
 	{
 		getcwd(buf, 1024);
 		chdir(tmp->content);
 		ft_free((void **)&tmp->content);
 		tmp->content = buf;
-		used = 1;
 	}
 	else if (mode == 0)
 	{
 		getcwd(buf, 1024);
 		ft_free((void **)&tmp->content);
 		tmp->content = buf;
-		used = 1;
 	}
-	if (used == 0)
+	if ((tmp->content && mode == 1) || mode == 0) 
 		ft_free((void **)&buf);
 	return (1);
 }
@@ -79,39 +85,33 @@ int		olddir(t_all *all, int *catch, int mode)
 {
 	t_lst	*tmp;
 	char	*buf;
-	int		f;
-	int		used;
 
-	f = 0;
-	used = 0;
 	tmp = all->alst;
-	buf = ft_calloc(sizeof(buf), 1024);
+	if (!(buf = ft_calloc(sizeof(buf), 1024)))
+	{
+		pwd_error(all, catch, 0);
+		return (0);
+	}
 	while (tmp)
 	{
 		if (!ft_strcmp(tmp->key, "OLDPWD"))
 		{
-			f = 1;
 			ft_free((void **)&buf);
 			return (pwdexist(all, catch, mode, tmp));
 		}
 		tmp = tmp->next;
 	}
-	if (f == 0 && mode == 0)
+	if (mode == 0)
 	{
 		getcwd(buf, 1024);
 		ft_lstadd_back_ms(&all->alst, ft_lstnew_ms(ft_strdup("OLDPWD"), buf));
-		used = 1;
 	}
 	else if (mode == 1)
-	{
-		all->err = 1;
-		*catch = 1;
-		error_msg("cd", "OLDPWD not set");
-	}
-	if (used == 0)
+		pwd_error(all, catch, 1);
+	if (mode == 0)
 		ft_free((void **)&buf);
 	return (1);
-}
+}//28: - 3 (+ unprotected lstadd back, lstnew and strdup)
 
 void	cd_chdir(char *path, int *catch, t_all *all)
 {
