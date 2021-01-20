eb378a3188a2cf70530d61f72053a5d552972a28
Jehanne DUSSERT <jdussert@student.42.fr>
2021-01-20 21:04:11 +0100

    Norme utils

--- srcs/ms_utils/ft_restablish_char.c
+++ srcs/ms_utils/ft_restablish_char.c
@@ -1,8 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_restablish_char.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jehannedussert <marvin@42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/20 20:57:56 by jehannedu         #+#    #+#             */
+/*   Updated: 2021/01/20 20:57:57 by jehannedu        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../includes/minishell.h"
 
-void    ft_replace(char *comm, char **new, int *i, int *j)
+void	ft_replace(char *comm, char **new, int *i, int *j)
 {
-    if (comm[*i] && comm[*i] == 1)
+	if (comm[*i] && comm[*i] == 1)
 		new[0][*j] = ' ';
 	else if (comm[*i] && comm[*i] == 2)
 		new[0][*j] = '<';
@@ -12,32 +24,32 @@ void    ft_replace(char *comm, char **new, int *i, int *j)
 		new[0][*j] = ';';
 	else if (comm[*i] && comm[*i] == 5)
 		new[0][*j] = '|';
-    (*j)++;
-    (*i)++;
+	(*j)++;
+	(*i)++;
 }
 
-void    ft_restablish_char(char ***comm)
+void	ft_restablish_char(char ***comm)
 {
-    int     i;
-    int     j;
-    char    *new;
+	int		i;
+	int		j;
+	char	*new;
 
-    i = 0;
-    j = 0 ;
-    if ((new = ft_calloc(ft_strlen((*comm)[0]) + 1, sizeof(char))) == NULL)
-        return ;
-    while ((*comm)[0][i])
-    {
-        if ((*comm)[0][i] == 4 || (*comm)[0][i] == 5)
-            ft_replace((*comm)[0], &new, &i, &j);
-        else
-        {
-            new[j] = (*comm)[0][i];
-            i++;
-            j++;
-        }
-    }
-    free_read(NULL, *comm);
+	i = 0;
+	j = 0;
+	if ((new = ft_calloc(ft_strlen((*comm)[0]) + 1, sizeof(char))) == NULL)
+		return ;
+	while ((*comm)[0][i])
+	{
+		if ((*comm)[0][i] == 4 || (*comm)[0][i] == 5)
+			ft_replace((*comm)[0], &new, &i, &j);
+		else
+		{
+			new[j] = (*comm)[0][i];
+			i++;
+			j++;
+		}
+	}
+	free_read(NULL, *comm);
 	if (new)
 	{
 		(*comm)[0] = ft_strdup(new);
@@ -45,29 +57,29 @@ void    ft_restablish_char(char ***comm)
 	}
 }
 
-void    ft_restablish_redir(char ***comm, t_all *all)
+void	ft_restablish_redir(char ***comm, t_all *all)
 {
-    int     i;
-    int     j;
-    char    *new;
+	int		i;
+	int		j;
+	char	*new;
 
-    i = 0;
-    j = 0 ;
-    if ((new = ft_calloc(all->cmd_len + 1, sizeof(char))) == NULL)
-        return ;
-    while ((*comm)[0][i])
-    {
-        if ((*comm)[0][i] == 1 || (*comm)[0][i] == 2 ||
-            (*comm)[0][i] == 3)
-            ft_replace((*comm)[0], &new, &i, &j);
-        else
-        {
-            new[j] = (*comm)[0][i];
-            i++;
-            j++;
-        }
-    }
-    free_read(NULL, *comm);
+	i = 0;
+	j = 0;
+	if ((new = ft_calloc(all->cmd_len + 1, sizeof(char))) == NULL)
+		return ;
+	while ((*comm)[0][i])
+	{
+		if ((*comm)[0][i] == 1 || (*comm)[0][i] == 2 ||
+			(*comm)[0][i] == 3)
+			ft_replace((*comm)[0], &new, &i, &j);
+		else
+		{
+			new[j] = (*comm)[0][i];
+			i++;
+			j++;
+		}
+	}
+	free_read(NULL, *comm);
 	if (new)
 	{
 		(*comm)[0] = ft_strdup(new);
@@ -97,8 +109,8 @@ void	ft_s_char(char **comm, char **new, int *i, int *j)
 void	ft_init_check_sep(int *i, int *j, int *d, int *s)
 {
 	*i = 0;
-    *j = 0;
-    *s = 0;
+	*j = 0;
+	*s = 0;
 	*d = 0;
 }
 
@@ -119,11 +131,11 @@ int		ft_check_bs(char *comm, int i)
 
 void	ft_check_sep(char **comm)
 {
-    int		i;
-    int		s;
+	int		i;
+	int		s;
 	int		d;
-    char	*new;
-    int		j;
+	char	*new;
+	int		j;
 
 	ft_init_check_sep(&i, &j, &d, &s);
 	if ((new = ft_calloc(ft_strlen(*comm) + 1, sizeof(char))) == NULL)
