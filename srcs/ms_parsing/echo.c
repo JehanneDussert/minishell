547eac545d6709bb7fce5be129b8b941a1e3be24
JehanneDussert <jdussert@student.42.fr>
2021-01-27 17:06:54 +0100

    Fixes echo -nnn word -nnn

--- srcs/ms_parsing/echo.c
+++ srcs/ms_parsing/echo.c
@@ -6,7 +6,7 @@
 /*   By: jdussert <jdussert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/20 13:18:45 by ede-banv          #+#    #+#             */
-/*   Updated: 2021/01/27 16:33:35 by jdussert         ###   ########.fr       */
+/*   Updated: 2021/01/27 17:05:47 by jdussert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ void	ft_check_n(int *i, int *res, char *comm, char **opt)
 	*res = ft_echo_n(comm, opt);
 	if (*res != 0)
 		(*i)++;
-	*res = 0;
+	*res = 1;
 }
 
 void	ft_echo(char **comm, t_all *all)
@@ -46,11 +46,15 @@ void	ft_echo(char **comm, t_all *all)
 	while (comm[i])
 	{
 		j = 0;
-		if (comm[i][j] == '-' && (i == 1 || opt[0] == '-'))
+		if (comm[i][j] == '-' && (i == 1 || opt[0] == '-') && res != 1)
 			ft_check_n(&i, &res, comm[i], &opt);
-		if (comm[i][j] != '-' || !ft_strncmp(opt, "off", 3))
-			while (comm[i][j] && comm[i][j] != 1)
-				ft_putchar_fd(comm[i][j++], 1);
+		if (res == 1 && !ft_strncmp(comm[i], "-n", 2))
+			ft_putchar_fd(' ', 1);
+		while (comm[i][j] && comm[i][j] != 1)
+		{
+			
+			ft_putchar_fd(comm[i][j++], 1);
+		}
 		if (comm[i + 1] && ft_strncmp(opt, "-n", 2))
 			ft_putchar_fd(' ', 1);
 		i++;
